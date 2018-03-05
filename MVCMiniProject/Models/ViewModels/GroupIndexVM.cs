246d367a11823967facfe63529ca234560be51be
Andreas Sundin <Andreasnr13@gmail.com>
2018-03-05 18:18:55 +0100

    Initial commit

---
+++ MVCMiniProject/Models/ViewModels/GroupIndexVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCMiniProject.Models.ViewModels
+{
+    public class GroupIndexVM
+    {
+        public string GroupName { get; set; }
+        public string Description { get; set; }
+        public string[] PersonName { get; set; }
+        public string[] TechName { get; set; }
+    }
+}
